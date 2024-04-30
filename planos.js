5b05897df294309532dfbe84dbbc685b3896aed2
Renan Massini <renan.massini00@gmail.com>
2024-04-30 11:46:03 -0300

    first commit

---
+++ planos.js
@@ -0,0 +1,41 @@
+let prataJson = [
+    {
+        id: 1,
+        name: '100 MEGA',
+        img: 'assets/Imagens/plano_100mb (2).jpg',
+        sizes: [
+          
+          
+        ],
+        description: 'Adequado para casas com múltiplos dispositivos conectados, jogos online e streaming de conteúdo em 4K. Oferece uma experiência de internet fluida e sem interrupções para famílias maiores ou usuários que exigem mais da sua conexão'
+      },
+      {
+        id: 2,
+        name: '200 MEGA',
+        img: 'assets/Imagens/plano_200mb.jpg',
+        sizes: [
+          
+        ],
+        description: 'Recomendado para residências com intensa atividade online, como teletrabalho, videoconferências e downloads de grandes arquivos.Garante uma conexão estável e de alta velocidade para suportar múltiplos usuários e dispositivos simultaneamente.'
+      },
+      {
+        id: 3,
+        name: '500 MEGA',
+        img: 'assets/Imagens/plano_500mb (2).jpg',
+        sizes: [
+          
+        ],
+        description: 'Projetado para clientes que demandam o máximo desempenho, como gamers entusiastas, empresas em crescimento e famílias com muitos dispositivos conectados.Oferece uma velocidade ultrarrápida para downloads, uploads e streaming de conteúdo em alta definição sem atrasos.'
+      },
+      {
+        id: 4,
+        name: '700 MEGA',
+        img: 'assets/Imagens/plano_700mb.jpg',
+        sizes: [
+          
+        ],
+        description: 'O plano premium para clientes que buscam o melhor em conectividade e desempenho.Ideal para empresas de grande porte, entusiastas de tecnologia e famílias que necessitam de uma conexão ultra-rápida e altamente confiável para todas as suas atividades online.'
+      }
+      
+    
+];
